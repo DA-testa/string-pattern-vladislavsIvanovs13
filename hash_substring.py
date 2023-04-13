8e655371e7a7ef25d5e927ea0d2068a8e2d553f7
vladIvanov13 <124610956+vladislavsIvanovs13@users.noreply.github.com>
2023-04-13 03:43:33 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,42 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    command = input()
+    if "F" in command:
+        file_name = input()
+        path = "tests/" + file_name
+        if not "a" in file_name:
+            choice = open(path, "r").read()
+            partitioned = choice.partition("\n")
+            choice.close()
+            pattern = partitioned[0].rstrip()
+            text = partitioned[2].rstrip()
+            tuple = tuple(pattern, text)
+            return tuple
+        
+        
+    elif "I" in command:
+        pattern = input().rstrip()
+        text = input().rstrip()
+        tuple = tuple(pattern, text)
+        return tuple
     
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
+    
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
+    
+    
+    for ch in text:
+        if ch == 
     # and return an iterable variable
     return [0]
 
 
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
 
